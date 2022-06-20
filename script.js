fb48f4b60c8217ecd1028898fc923edf39b75b1a
jonatas1565 <jonatasferreira330@gmail.com>
2022-06-20 17:58:22 -0300

    feat: ScrollView

---
+++ script.js
@@ -0,0 +1,8 @@
+window.sr = ScrollReveal({reset: true})
+sr.reveal('.box-lateral',  {duration: 1600});
+sr.reveal('.Titulo-projetos', {duration: 1600})
+sr.reveal('.card',  {
+    reveal: {x: 100, y:1000, z:0},
+    duration: 3600,
+
+});
